12d1b5e904b2b21ba33a50b81053633b7bc1df2f
Ahsan <ahsanfarooq210@gmail.com>
2020-07-29 20:51:31 +0500

    Add project files.

---
+++ leetCode_reverse_integer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode_reverse_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(Reverse(num));
+            Console.ReadLine();
+
+        }
+        public static int Reverse(int x)
+        {
+            long reversed = 0;
+            while (x != 0)
+            {
+                reversed = reversed * 10 + x % 10;
+                x /= 10;
+            }
+            if (reversed > int.MaxValue || reversed < int.MinValue) return 0;
+            return (int)reversed;
+        }
+    }
+}
